1169ffeb227cb494ee3de3063ac729c0e24bbd08
aikoschurmann <aiko.schurmann@gmail.com>
2026-09-14 17:13:32 +0200

    Switch to CMake

---
+++ src/config.c
@@ -0,0 +1,4 @@
+#include <stdbool.h>
+
+#define CONFIG_IMPLEMENTATION
+#include "scone_config.h"
--- src/main.c
+++ src/main.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-#define CONFIG_IMPLEMENTATION
 #include "scone_config.h"
 
 int main(int argc, const char **argv) {
     SconeConfig_t cfg;
     cfg_error_t err;
 
-    // Load from config/scone.ini, environment variables, and CLI arguments
-    if (SconeConfig_load(&cfg, "config/scone.ini", argc, argv, &err) == CFG_SUCCESS) {
-        printf("Configuration loaded successfully!\n");
-        printf("Cost (Max AST Depth): %d\n", (int)cfg.cost);
-        printf("Threads: %d\n", (int)cfg.threads);
-        SconeConfig_free(&cfg);
-    } else {
-        fprintf(stderr, "Configuration error in '%s': %s\n", err.field, err.message);
-        return 1;
+    if (SconeConfig_load(&cfg, "config/scone.ini", argc, argv, &err) != CFG_SUCCESS) {
+        fprintf(stderr, "FATAL: Configuration error in '%s': %s\n", err.field, err.message);
+        return EXIT_FAILURE;
     }
 
-    return 0;
+    printf("Starting Scone...\n");
+    printf(" - Threads: %d\n", (int)cfg.threads);
+    printf(" - Cost (Max AST Depth): %d\n", (int)cfg.cost);
+    
+    // TODO: Add core application logic here
+
+    SconeConfig_free(&cfg);
+    return EXIT_SUCCESS;
 }
